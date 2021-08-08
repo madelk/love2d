42f7f485375a814a2c079e409a20ddecc187b098
Mark Dell <madelk@gmail.com>
2021-08-08 17:21:50 -0500

    pewpew

--- spaceshooter/main.lua
+++ spaceshooter/main.lua
@@ -3,7 +3,7 @@ function love.load()
     window.x = 400
     window.y = 240
     gameSettings = {}
-    gameSettings.chanceOfStar = 5
+    gameSettings.chanceOfStar = 2
     stars = {}
     for i = 1, window.x do
         addStarMaybe(i)
@@ -12,35 +12,42 @@ function love.load()
     score = 0
     timer = 60
     gameFont = love.graphics.newFont(40)
-    love.window.setMode(window.x, window.y, {})
     love.graphics.setDefaultFilter("nearest", "nearest", 0)
     img = love.graphics.newImage("spritemap.png")
-    love.graphics.scale(2, 2)
     player = {}
     player.x = window.x / 4
     player.y = window.y / 2
     player.speed = 0.4
-
+    player.bullets = {}
+    player.bulletSpeed = 2
     ship = love.graphics.newQuad(0, 0, 16, 16, img:getDimensions())
+    bullet = love.graphics.newQuad(16, 0, 16, 16, img:getDimensions())
 end
 
 function addStarMaybe(x)
     if love.math.random(0, 100) < gameSettings.chanceOfStar then
-        local y = love.math.random(0, window.y) 
-        table.insert(stars,{x, y})
+        local y = love.math.random(0, window.y)
+        table.insert(stars, {x, y})
     end
 end
 
 function love.update(dt)
-    if stars[1][1] < 0 then
-        table.remove(stars, 1)
-    end
-
     for k, v in pairs(stars) do
         local newx = v[1] - player.speed
         stars[k] = {newx, v[2]}
-      end
+        if newx < 0 then
+            table.remove(stars, k)
+        end
+    end
     addStarMaybe(window.x)
+
+    for k, v in pairs(player.bullets) do
+        local newx = v[1] + player.bulletSpeed
+        player.bullets[k] = {newx, v[2]}
+        if newx > window.x then
+            table.remove(player.bullets, k)
+        end
+    end
 
     if love.keyboard.isDown("up") then
         player.y = player.y - player.speed
@@ -54,9 +61,15 @@ function love.update(dt)
     if love.keyboard.isDown("right") then
         player.x = player.x + player.speed
     end
+    if love.keyboard.isDown("space") then
+        table.insert(player.bullets, {player.x, player.y})
+    end
 end
 
 function love.draw()
+    for k, v in pairs(player.bullets) do
+        love.graphics.draw(img, bullet, v[1], v[2], math.pi / 2)
+    end
     love.graphics.points(stars)
     love.graphics.draw(img, ship, player.x, player.y, math.pi / 2)
     beWhite()
