a5fe7eb47f1b0581dc4b2936f7f6a2370529a240
Mark Dell <madelk@gmail.com>
2021-08-08 22:49:38 -0500

    Use delta time like a good boi

--- spaceshooter/main.lua
+++ spaceshooter/main.lua
@@ -4,7 +4,7 @@ function love.load()
     window.y = 240
     gameSettings = {}
     gameSettings.chanceOfStar = 2
-    gameSettings.scrollSpeed = 0.5
+    gameSettings.scrollSpeed = 50
     stars = {}
     for i = 1, window.x do
         addStarMaybe(i)
@@ -17,10 +17,10 @@ function love.load()
     player = {}
     player.x = window.x / 4
     player.y = window.y / 2
-    player.speed = 0.6
+    player.speed = 60
     player.bullets = {}
-    player.bulletSpeed = 2
-    player.bulletCooldown = 50
+    player.bulletSpeed = 200
+    player.bulletCooldown = 25
     player.bulletCooldownTimer = 0
     player.shipsize = 16
     player.boundary = {}
@@ -32,7 +32,29 @@ function love.load()
     bullet = love.graphics.newQuad(16, 0, 16, 16, img:getDimensions())
     alien = love.graphics.newQuad(32, 0, 16, 16, img:getDimensions())
     enemies = {{410, 20}, {420, 200}}
+    enemiesSpeed = 10;
     visibleEnemies = {}
+    createParticle()
+end
+
+function createParticle()
+	-- Create a simple image with a single white pixel to use for the particles.
+	-- We could load an image from the hard drive but this is just an example.
+	local imageData = love.image.newImageData(1, 1)
+	imageData:setPixel(0,0, 1,1,1,1)
+
+	local image = love.graphics.newImage(imageData)
+
+	-- Create and initialize the particle system object.
+	particleSystem = love.graphics.newParticleSystem(image, 1000)
+	particleSystem:setEmissionRate(50)
+    particleSystem:setLinearAcceleration( -500, 0, -1000, 0 )
+	particleSystem:setParticleLifetime(.2, .5)
+	particleSystem:setDirection(1*math.pi)
+	particleSystem:setSizes(1)
+	particleSystem:setSpread(.5*math.pi)
+	particleSystem:setSpeed(50, 80)
+	particleSystem:setColors(1,1,1,1)
 end
 
 function addStarMaybe(x)
@@ -42,9 +64,9 @@ function addStarMaybe(x)
     end
 end
 
-function updateStars()
+function updateStars(dt)
     for k, v in pairs(stars) do
-        local newx = v[1] - gameSettings.scrollSpeed
+        local newx = v[1] - gameSettings.scrollSpeed * dt
         stars[k] = {newx, v[2]}
         if newx < 0 then
             table.remove(stars, k)
@@ -53,12 +75,12 @@ function updateStars()
     addStarMaybe(window.x)
 end
 
-function updatePlayerBullets()
+function updatePlayerBullets(dt)
     if player.bulletCooldownTimer > 0 then
         player.bulletCooldownTimer = player.bulletCooldownTimer - 1
     end
     for k, v in pairs(player.bullets) do
-        local newx = v[1] + player.bulletSpeed
+        local newx = v[1] + player.bulletSpeed * dt
         player.bullets[k] = {newx, v[2]}
         if newx > window.x then
             table.remove(player.bullets, k)
@@ -66,18 +88,18 @@ function updatePlayerBullets()
     end
 end
 
-function handlePlayerInput()
+function handlePlayerInput(dt)
     if love.keyboard.isDown("up") and player.y >= player.boundary.ymin then
-        player.y = player.y - player.speed
+        player.y = player.y - player.speed * dt
     end
     if love.keyboard.isDown("down") and player.y <= player.boundary.ymax then
-        player.y = player.y + player.speed
+        player.y = player.y + player.speed * dt
     end
     if love.keyboard.isDown("left") and player.x >= player.boundary.xmin then
-        player.x = player.x - player.speed
+        player.x = player.x - player.speed * dt
     end
     if love.keyboard.isDown("right") and player.x <= player.boundary.xmax then
-        player.x = player.x + player.speed
+        player.x = player.x + player.speed * dt
     end
     if love.keyboard.isDown("space") and player.bulletCooldownTimer == 0 then
         table.insert(player.bullets, {player.x, player.y})
@@ -85,9 +107,9 @@ function handlePlayerInput()
     end
 end
 
-function updateEmenies()
+function updateEmemies(dt)
     for k, v in pairs(visibleEnemies) do
-        local newx = v[1] - 1
+        local newx = v[1] - (gameSettings.scrollSpeed + enemiesSpeed) * dt
         visibleEnemies[k] = {newx, v[2]}
         if newx < 0 then
             table.remove(visibleEnemies, k)
@@ -113,12 +135,17 @@ function collisionDetection()
     end
 end
 
+function updatePlayerParticle(dt)
+	particleSystem:moveTo(player.x-16, player.y+8)
+	particleSystem:update(dt) -- This performs the simulation of the particles.
+end
+
 function love.update(dt)
-    updateEmenies()
+    updateEmemies(dt)
     collisionDetection()
-    updateStars()
-    updatePlayerBullets()
-    handlePlayerInput()
+    updateStars(dt)
+    updatePlayerBullets(dt)
+    handlePlayerInput(dt)
     screenPosition = screenPosition + gameSettings.scrollSpeed
 end
 
@@ -127,6 +154,8 @@ function CheckCollision(x1, y1, w1, h1, x2, y2, w2, h2)
 end
 
 function love.draw()
+    beWhite()
+	love.graphics.draw(particleSystem)
     love.graphics.print(score, 0, 0)
 
     for k, v in pairs(visibleEnemies) do
@@ -138,7 +167,6 @@ function love.draw()
     end
     love.graphics.points(stars)
     love.graphics.draw(img, ship, player.x, player.y, math.pi / 2)
-    beWhite()
     love.graphics.setFont(gameFont)
 end
 
